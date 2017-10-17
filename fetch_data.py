cad2ae689f59d675eadf4e8e183ad10c5ae73469
andyparadise <davison.andy@gmail.com>
2017-10-17 13:10:53 -0300

    Second

--- fetch_data.py
+++ fetch_data.py
@@ -1,1 +1,23 @@
-# this is a comment
+# Fetches data from url then write to file
+
+def get_date(movie1, movie2, movie3):
+
+    import scrapy
+
+    descrip1 = scrapy.spider(movie1)
+    descrip2 = scrapy.spider(movie2)
+    descrip3 = scrapy.spider(movie3)
+
+    html1 = descrip1.read()
+    html2 = descrip2.read()
+    html3 = descrip3.read()
+
+    from bs4 import BeautifulSoup
+
+    soup1 = BeautifulSoup(html1, 'html.parser').get_text()
+    soup2 = BeautifulSoup(html2, 'html.parser').get_text()
+    soup3 = BeautifulSoup(html3, 'html.parser').get_text()
+
+    final = soup1 + soup2 + soup4
+
+    return final
--- run.py
+++ run.py
@@ -1,7 +1,13 @@
-
-
-
+import fetch_data
 
 from markov_python.cc_markove import MarkovChain
 
+movie1 = raw_input("Input link to lyrics: ")
+movie2 = raw_input("Input link to lyrics: ")
+movie3 = raw_input("Input link to lyrics: ")
+
 mc = MarkovChain()
+
+mc.add_string(fetch_data.get_data(movie1, movie2 movie3))
+
+print mc.generate_text()
